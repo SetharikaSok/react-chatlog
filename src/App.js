33a198aeb6d06d3d6063aea32752b1cfee601e6d
Lindsay <lindsaybolzwilson@gmail.com>
2023-06-20 13:15:25 -0400

    Wave 1 -- connected one element to chat entry

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import './components/ChatEntry';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const chatMessage = chatMessages[0]
+  console.log(chatMessage)
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry 
+          body={chatMessage.body}  
+          sender={ chatMessage.sender } 
+          timeStamp={ chatMessage.timeStamp }>
+        </ChatEntry>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
